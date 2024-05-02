813ba0baf9ca503cdba79060e04063667806d820
randeeshapalliyaguruge <cb009991@students.apiit.lk>
2024-05-02 17:44:40 +0530

    update track edit

--- src/views/admin/track/Edit.vue
+++ src/views/admin/track/Edit.vue
@@ -134,6 +134,25 @@
                           </div>
                         </div>
 
+                        <div class="sm:col-span-8">
+                          <label
+                            for="file_path"
+                            class="block text-sm font-medium leading-6 text-gray-900">
+                            File Path
+                          </label>
+                          <div class="mt-2">
+                            <div
+                              class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                              <input
+                                type="text"
+                                name="file_path"
+                                id="file_path"
+                                class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                v-model="track.file_path"/>
+                            </div>
+                          </div>
+                        </div>
+
                         <div class="sm:col-span-3">
                           <label
                             for="sort_order"
