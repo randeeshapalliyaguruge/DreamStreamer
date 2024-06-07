0ca3a15b59c31d4e7c390c3f43719cb66a5310ff
randeeshapalliyaguruge <cb009991@students.apiit.lk>
2024-06-07 21:11:40 +0530

    system updates

--- src/router/index.js
+++ src/router/index.js
@@ -23,14 +23,6 @@ const router = createRouter({
       name: 'login',
       component: () => import('../views/LoginView.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
     // {
     //   path: '/albums',
     //   name: 'albums',
